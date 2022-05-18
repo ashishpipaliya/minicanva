72743de6f298045eca3f4f239ba9078d37821d6b
ashishpipaliya <ashish.pipaliya22@gmail.com>
2022-05-18 21:01:32 +0530

    ♥ added sidebar

--- lib/layout/sidebar.dart
+++ lib/layout/sidebar.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:quotesmaker/provider/drawer_provider.dart';
-
 
 class SideBar extends StatefulWidget {
   const SideBar({Key? key}) : super(key: key);
